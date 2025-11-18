197afd7427eea86de6e0521cf8363adc4e10eafa
bani <dalllo9851@gmail.com>
2025-11-18 21:49:39 +0900

    v-if, v-show

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
-  <h1>채널명 : {{ channel }}</h1>
-  <!-- 머스태시 : 콧수염 문법 -->
-  <!-- text interpolation 보간법  -->
-  <h2>구독자수 : {{ subscribe }}</h2>
-  <p>오늘의 행운 숫자 : {{ Math.ceil(Math.random() * 45) }}</p>
-  <h1 v-text="channel"></h1>
-  <h2 v-text="subscribe"></h2>
-  <!-- <h2 v-text="subscribe">123</h2> 오류 -->
-
-  <p v-html="channel2"></p>
-  <!-- v-text 에는 html이 표시되어버림 , v-html은 html 문법 적용가능 -->
+  <h1 v-if="score >= 90">평점 : A</h1>
+  <h1 v-else-if="score >= 80">평점 : B</h1>
+  <h1 v-else-if="score >= 70">평점 : C</h1>
+  <h1 v-else>평점 : D</h1>
+  <h1 v-if="score != 60">이 영역은 v-if에서 관리</h1>
+  <h1 v-show="score != 60">이 영역은 v-show에서 관리</h1>
+  <!-- v-show는 개발자 도구에서 false라도 확인 가능 -->
+  <!-- 자주 변경되면 v-show, 변경 안되면 v-if -->
 </template>
 
 <script>
@@ -17,7 +14,7 @@ export default {
   data() {
     return {
       channel: "홈페이지 만들기",
-      channel2: "<b>홈페이지2 </b>",
+      score: 60,
       subscribe: 2677,
     };
   },
