e3800e969bad47e82e692413e495be27ea157d25
bani <dalllo9851@gmail.com>
2025-11-20 21:52:34 +0900

    [20251120] v-model 상품추가

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,58 @@
 <template>
-  <div
-    id="display"
-    v-on:mousemove="mousePos"
-    v-on:click.right.prevent="mouseClick"
-  >
-    x : {{ x }} <br />
-    y : {{ y }}
+  <div>
+    <!-- <form v-on:submit.prevent="addItem"> -->
+      <form>
+      <p>품목 : <input type="text" required v-model="itemName"></input></p>
+      <p>수량 : <select v-model="itemCnt">
+        <option value="1">1개</option>
+        <option value="2">2개</option>
+        <option value="3">3개</option>
+        <option value="4">4개</option>
+        <option value="5">5개</option>
+      </select></p>
+      <button v-on:click.prevent="btnAddItem">상품추가</button>
+    </form>
+    <p> 구입할 품목 : </p>
+    <ul>
+      <li v-for="item in sList">{{ item.name }} ({{ item.cnt }}개)</li>
+    </ul>
   </div>
-  <textarea>{{ msg }}</textarea>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      x: 0,
-      y: 0,
-      msg: "",
-    };
+      itemName: null,
+      itemCnt: 1,
+      sList: [
+        { name : '감귤', cnt: 2}
+      ]
+    }
   },
   methods: {
-    mousePos(e) {
-      this.x = e.offsetX;
-      this.y = e.offsetY;
+    addItem() {
+      let item = {
+        name: this.itemName,
+        cnt : this.itemCnt,
+      }
+      this.sList.push(item);
+      this.itemName = null;
+      this.itemCnt = 1
     },
-    mouseClick(e) {
-      this.msg += this.x + "," + this.y + " \n";
-    },
-  },
-};
+    btnAddItem() {
+       let item = {
+        name: this.itemName,
+        cnt : this.itemCnt,
+      }
+      this.sList.push(item);
+      this.itemName = null;
+      this.itemCnt = 1
+    }
+  }
+}
 </script>
 
-<style>
-#display {
-  width: 200px;
-  height: 200px;
-  background-color: goldenrod;
-}
-textarea {
-  height: 500px;
-}
+<style lang="scss" scoped>
+
 </style>
