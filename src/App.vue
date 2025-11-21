5f717e493652748cc845edd151ed3d24d314499e
bani <dalllo9851@gmail.com>
2025-11-21 20:49:33 +0900

    [20251121] style 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <div style="padding: 20px;">
-    <h1>현재 변수 값: {{ myText }}</h1>
-    <hr />
-
-    <h3>1. v-bind (:value) - 짝사랑</h3>
-    <p>여기에 글을 써도 위쪽 '변수 값'은 안 변해요!</p>
-    <input :value="myText" style="padding: 5px;"></input>
-
-    <br /><br />
-
-    <h3>2. v-model - 서로 사랑</h3>
-    <p>여기에 글을 쓰면 '변수 값'이 실시간으로 변해요</p>
-    <input v-model="myText" style="padding: 5px;"></input>
-  </div>
+  <!--  inline 스타일 > 클래스 스타일 -->
+  <div
+    class="demo"
+    @mouseover="initWidth"
+    :style="{ width: myWidth + 'px' }"
+  ></div>
+  <button @click="incWidth">버튼을 클릭해보세요</button>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      myText: "초기값"
-    }
-  }
-}
+      myWidth: 10,
+    };
+  },
+  methods: {
+    incWidth: function () {
+      this.myWidth += 10;
+    },
+    initWidth: function () {
+      this.myWidth = 10;
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
+<style>
+.demo {
+  width: 100px;
+  height: 100px;
+  background-color: blueviolet;
+}
 </style>
