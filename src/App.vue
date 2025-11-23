223fc228352df70e96f077342e0e5ae2daf8747a
bani <dalllo9851@gmail.com>
2025-11-23 21:50:44 +0900

    [20251123] provide , inject

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <div>
-    <h1>나는 부모 컴포넌트 ({{ c }})</h1>
-    <!-- 자식 컴포넌트에서 emit으로 정의를 한다 -->
-    <!-- 부모 컴포넌트에서는 자식컴포넌트를 쓰고 있는 요소에 @text 적은 후 함수를 정의한다. -->
-    <LocalChild @create-post="callFunction" />
+    <h1>안녕하세요.</h1>
+    <FirstChild />
   </div>
 </template>
 
 <script>
-import LocalChild from "./components/LocalChild.vue";
+import FirstChild from "./components/FirstChild.vue";
 
 export default {
   data() {
     return {
-      c: 0,
+      gift: "장난감",
     };
   },
-
   components: {
-    LocalChild,
+    FirstChild,
   },
-  methods: {
-    callFunction(var1) {
-      console.log("자식 컴포넌트에서 보낸 이벤트를 수신했어요" + var1);
-      this.c = var1;
-    },
+  provide() {
+    return {
+      gift: this.gift,
+    };
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+h1 {
+  color: red;
+}
+span {
+  margin-left: 20px;
+}
+</style>
