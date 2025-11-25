e6e9fd5bca844efa3a1cd7875ed47be3fa2757ca
bani <dalllo9851@gmail.com>
2025-11-25 22:13:35 +0900

    [20251125] deep, immediate

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
   <div>
-    <p>cnt : {{ cnt }}</p>
-    <p>old : {{ obj.oldValue }} / new : {{ obj.newValue }}</p>
-    <button @click="counter">클릭</button>
+    <h1>Profile</h1>
+    <p>Name : {{ obj.name }}</p>
+    <p>Age : {{ obj.age }}</p>
+    <button @click="updateName">이름 변경</button>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive, watch } from "vue";
+import { ref, watch } from "vue";
 
-const cnt = ref(1);
-const obj = reactive({
-  oldValue: null,
-  newValue: null,
-});
-const counter = () => cnt.value++;
+const obj = ref({ name: "홍길동", age: 30 });
+const updateName = () => (obj.value.name = "도경우");
 
-watch(cnt, (newValue, oldValue) => {
-  obj.oldValue = oldValue;
-  obj.newValue = newValue;
-});
+// 참조객체는 obj의 주소를 담고 있기때문에 주소가 바뀐게 아니라 감지를 못함
+// 내부적으로 변경된건 감지 못함
+// 감지를 하기위한 조치 deep : true
+watch(
+  obj,
+  (newObj) => {
+    console.log("이름 변경을 감지했습니다. 변경된 이름 : " + newObj.name);
+  },
+  { deep: true }
+);
 </script>
 
 <style lang="scss" scoped></style>
