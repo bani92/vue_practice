c135ec888223deb46c2c14aa7d317c8126df62d2
bani <dalllo9851@gmail.com>
2025-12-03 21:20:44 +0900

    [20251203] defineEmit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <h1>아래 있는 컴포넌트는 자식 컴포넌트입니다.</h1>
-  <ButtonCounter :title="post.title" :mainCont="post.mainCont" />
+  <div>
+    <ChildComponent @customEvent="handleCustomEvnet" />
+  </div>
 </template>
 
 <script setup>
-import ButtonCounter from "./components/ButtonCounter.vue";
+import ChildComponent from "./components/ChildComponent.vue";
 
-const post = {
-  id: 1,
-  title: "이것은 제목입니다.",
-  mainCont: "이것은 본문입니다.",
+const handleCustomEvnet = (message) => {
+  console.log("Received event: ", message);
 };
 </script>
 
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button @click="handleClick">Click me</button>
+  </div>
+</template>
+
+<script setup>
+import { defineEmits } from "vue";
+
+const emit = defineEmits(["customEvent"]);
+const handleClick = () => {
+  emit("customEvent", "Button clicked!");
+};
+</script>
+
+<style lang="scss" scoped></style>
