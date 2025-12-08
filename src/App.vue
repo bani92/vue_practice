bfaa00971eae8577f60be70910c71226891209e2
bani <dalllo9851@gmail.com>
2025-12-08 21:40:16 +0900

    [20251208] v-for, 수정, 삭제

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <button @click="getData" class="btn btn-success ms-3 mt-3">데이터 가져오기</button>
     <div v-show="isInputMode">
       <form autocomplete="off">
+        
         <label class="form-level">Name</label>
         <input type="text" class="form-control" v-model="name">
         <label class="form-level">Email</label>
@@ -14,7 +15,29 @@
       </form>
     </div>
 
-    <div ref="table"></div>
+    <div ref="table">
+      <table class="table ms-3">
+        <thead>
+          <tr>
+          <th>id</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>(*)</th>
+          </tr>
+        </thead>
+      <tbody>
+        <tr v-for="member in members" :key="member.id">
+          <td>{{ member.id }}</td>
+          <td>{{ member.name }}</td>
+          <td>{{ member.email }}</td>
+          <td>
+            <button type="button" @click="getEachData(member.id)" class="btn btn-primary me-3">수정</button>
+            <button type="button" @click="deleteData(member.id)" class="btn btn-danger">삭제</button>
+          </td>
+        </tr>
+      </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -26,47 +49,66 @@ const table = ref(null);
 const isInputMode = ref(false);
 const name = ref('')
 const email = ref('')
-
+const members = ref([])
+const member_id = ref('')
 
 const inputData = async () => {
-  const res = await axios
-    .post("http://localhost:3000/member", {
-      name: name.value,
-      email: email.value,
-    })
-    .then((res) => {
-      console.log(res);
-    });
-  name.value = '';
-  email.value = '';
-  isInputMode.value = false;
-  getData();
+  try {
+
+    if (member_id.value == '') {
+      const res = await axios.post("http://localhost:3000/member", {
+        name: name.value,
+        email: email.value,
+      });
+    } else {
+      const res = await axios.put("http://localhost:3000/member/" + member_id.value, {
+        name: name.value,
+        email: email.value,
+      });
+    }
+    name.value = '';
+    email.value = '';
+    member_id.value = '';
+    isInputMode.value = false;
+    getData();
+  } catch (err) {
+    console.log('서버에 이상이 있습니다.')
+  }
+  
+
 };
 
-const getData = () => {
-  axios.get("http://localhost:3000/member").then((res) => {
-    // console.log(res);
-    const members = res.data;
-    let htmlContent = `<table border="1">
-      <tr>
-         <th>id</th>
-         <th>Name</th>
-         <th>Email</th>
-      </tr>
-      `;
-    members.forEach((member) => {
-      htmlContent += `
-        <tr>
-          <td>${member.id}</td>
-          <td>${member.name}</td>
-          <td>${member.email}</td>
-        </tr>
-      `;
-    });
-    htmlContent += "</table>";
-    table.value.innerHTML = htmlContent;
-  });
+const getData = async () => {
+  try {
+    const res = await axios.get("http://localhost:3000/member")
+    members.value = res.data
+  } catch (err) {
+    console.log('서버에 이상이 있습니다.');
+  }
+  
 };
+
+const getEachData = async (id) => {
+  const res = await axios.get("http://localhost:3000/member/" + id)
+  name.value = res.data.name;
+  email.value = res.data.email;
+  member_id.value = res.data.id;
+  isInputMode.value = true;
+}
+
+const deleteData = async (id) => {
+  if (!confirm('삭제하시겠습니까')) {
+    return false;
+  }
+
+  try {
+    await axios.delete('http://localhost:3000/member/' + id)
+    getData();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 </script>
 
 <style lang="scss" scoped></style>
